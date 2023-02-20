b4091a6c29ad0ce5e05eab5451ab3d437733cce5
ruini1 <89319877+ruini1@users.noreply.github.com>
2023-02-20 03:45:49 -0500

    Update Cake.lua

--- Cake.lua
+++ Cake.lua
@@ -1,16 +1,11 @@
--- v3.0.3a
+-- v3.0.3aa
 
 --[[ DEVLOG ]]
 --[[
-# added a walkspeed bypass after so long but the reason it took so wrong was because i wrote it in the wrong place (the start of the script)
-# polished script up (still bad)
-# thats all]]
---[[ future stuff
-# blatant, truely blatant and nonblatant mode:
-	BLATANT MODE: changes the reach sliders cap to 40
-	NONBLATANT: caps the reach slider to 10 
-	TRUELY BLATANT: caps the reach slider to 300 and unlocks some features in the script such as the movement tab (more coming soon)
-	]]
+
+# devlog moved to github
+https://github.com/ruini1/cake
+]]
 
 	-- thats all for today, bye!! <3
 local p = game.Players.LocalPlayer; -- applying bypasses at the start so the walkspeed can work
